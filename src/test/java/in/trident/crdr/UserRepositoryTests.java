abddfce9aa3633645c291da917c5508734b44165
Nandha <nandha.kumar790@gmail.com>
2021-03-30 15:44:47 +0530

    removed daybal repos and classes

--- src/test/java/in/trident/crdr/UserRepositoryTests.java
+++ src/test/java/in/trident/crdr/UserRepositoryTests.java
@@ -6,44 +6,28 @@ package in.trident.crdr;
  */
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.List;
+import javax.persistence.EntityManager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.data.jpa.repository.query.AbstractJpaQuery;
 import org.springframework.test.annotation.Rollback;
 
-import in.trident.crdr.entities.DayBal;
-import in.trident.crdr.entities.Daybook;
-import in.trident.crdr.entities.DaybookBalance;
 import in.trident.crdr.entities.User;
-import in.trident.crdr.repositories.DaybookBalRepository;
-import in.trident.crdr.repositories.DaybookCRepository;
-import in.trident.crdr.repositories.DaybookRepository;
 import in.trident.crdr.repositories.UserRepository;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 @Rollback(false)
 public class UserRepositoryTests {
-
+	
 	@Autowired
-    private TestEntityManager entityManager;
-     
-    @Autowired
-    private UserRepository repo;
-
-    @Autowired
-    private DaybookRepository daybookRepo;
-
-    @Autowired
-    private DaybookBalRepository dbalrepo;
+	private UserRepository repo;
+	@Autowired
+	private EntityManager entityManager;
     
-    /*
     @Test
     public void testCreateUser() {
     	User user = new User();
@@ -57,14 +41,7 @@ public class UserRepositoryTests {
     	
     	assertThat(user.getEmail().equals(user2.getEmail()));
  
-    } */
+    } 
     
-    @Test
-    public void testDaybookBal() {
-    	DayBal dbbal = dbalrepo.findDaybookBalance("2020-04-15");
-    	System.out.println("<<<<<<<<<<<<<<<<<<<<<<<<");
-    	System.out.println(dbbal);
-    	System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>");
-    	
-    }
+   
 }
