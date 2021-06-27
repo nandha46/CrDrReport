f2ab025b412e11a25ceae4d1948c05da4c3abf56
Nandha <nandha.kumar790@gmail.com>
2021-06-28 00:25:21 +0530

    small edits

--- src/main/java/in/trident/crdr/models/BalanceSheetView.java
+++ src/main/java/in/trident/crdr/models/BalanceSheetView.java
@@ -11,5 +11,32 @@ package in.trident.crdr.models;
  */
 
 public class BalanceSheetView {
-
+	
+	private String particulars;
+	private String debit;
+	private String credit;
+	
+	public String getParticulars() {
+		return particulars;
+	}
+	public void setParticulars(String particulars) {
+		this.particulars = particulars;
+	}
+	public String getDebit() {
+		return debit;
+	}
+	public void setDebit(String debit) {
+		this.debit = debit;
+	}
+	public String getCredit() {
+		return credit;
+	}
+	public void setCredit(String credit) {
+		this.credit = credit;
+	}
+	
+	@Override
+	public String toString() {
+		return "BalanceSheetView [particulars=" + particulars + ", debit=" + debit + ", credit=" + credit + "]";
+	}	
 }
