48d24c77c4ee8abaa6a1406d84146c7412b3c123
Nandha <nandha.kumar790@gmail.com>
2021-09-16 22:49:59 +0530

    small changes

--- src/main/java/in/trident/crdr/services/LedgerServiceImpl.java
+++ src/main/java/in/trident/crdr/services/LedgerServiceImpl.java
@@ -27,6 +27,7 @@ import in.trident.crdr.models.LedgerForm;
 import in.trident.crdr.models.LedgerView;
 import in.trident.crdr.repositories.AccHeadRepo;
 import in.trident.crdr.repositories.DaybookRepository;
+
 /**
  * 
  * @author Nandhakumar Subramanian
@@ -36,149 +37,150 @@ import in.trident.crdr.repositories.DaybookRepository;
 
 @Service
 public class LedgerServiceImpl implements LedgerService {
-	
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(LedgerServiceImpl.class);
-	
+
 	@Autowired
 	private AccHeadRepo accHeadRepo;
-	
+
 	@Autowired
 	private DaybookRepository daybookRepo;
-	
+
 	SimpleDateFormat outsdf = new SimpleDateFormat("dd-MM-yyyy");
 	SimpleDateFormat insdf = new SimpleDateFormat("yyyy-MM-dd");
 	LocalizedNumberFormatter nf = NumberFormatter.withLocale(new Locale("en", "in"))
 			.precision(Precision.fixedFraction(2));
-	
+
 	@Override
 	public List<LedgerView> createLedgerViewList(LedgerForm ledgerForm) {
 		Profiler profiler = new Profiler("LedgerServiceImpl");
 		profiler.setLogger(LOGGER);
 		profiler.start("LedgerService");
 		List<LedgerView> ledgerList = new LinkedList<LedgerView>();
-		if(ledgerForm.isReportOrder()) {
+		if (ledgerForm.isReportOrder()) {
 			List<Integer> accCodes = ledgerForm.getAccCode();
 			accCodes.forEach(code -> {
-				LedgerView ledgerView =  createLedgerView(code,ledgerForm);
+				LedgerView ledgerView = createLedgerView(code, ledgerForm);
 				ledgerList.add(ledgerView);
 			});
 		} else {
 			Set<Integer> accCodes = new HashSet<Integer>(accHeadRepo.findAccCodes());
 			accCodes.remove(0);
 			accCodes.forEach(code -> {
-				LedgerView ledgerView =  createLedgerView(code,ledgerForm);
+				LedgerView ledgerView = createLedgerView(code, ledgerForm);
 				ledgerList.add(ledgerView);
 			});
 		}
 		LOGGER.debug("Ledger Created");
-		TimeInstrument ti =  profiler.stop();
+		TimeInstrument ti = profiler.stop();
 		LOGGER.info("\n" + ti.toString());
 		return ledgerList;
 	}
-	
+
 	@Override
 	public LedgerView createLedgerView(Integer code, LedgerForm ledgerForm) {
 		LedgerView ledgerview = new LedgerView();
 		ledgerview.setAccheadName(accHeadRepo.findAccNameByAccCode(code));
-		String arr[] = findOpeningBal(code,ledgerForm);
- 		Date date1 = new Date();
- 		try {
+		String arr[] = findOpeningBal(code, ledgerForm);
+		Date date1 = new Date();
+		try {
 			date1 = insdf.parse(ledgerForm.getStartDate());
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
-		
-		ledgerview.setDate(outsdf.format(date1));
- 		Double bal = new Double(arr[0]);
- 		
- 		if(arr[1].equals("Cr")) {
- 			ledgerview.setdOrC(nf.format(bal).toString());
- 			bal *= -1;
- 		} else {
- 			ledgerview.setOpeningBal(nf.format(bal).toString());
- 		}
- 		List<Dailybooks> dailybooklist = createDailybooks(code,ledgerForm.getStartDate(),ledgerForm.getEndDate(),bal);
- 		Double crT =  dailybooklist.stream().collect(Collectors.summingDouble(d-> new Double(d.getCreditAmt().replace(",", ""))));
- 		Double drT = dailybooklist.stream().collect(Collectors.summingDouble(d-> new Double(d.getDebitAmt().replace(",", ""))));
- 		
- 		if(arr[1].equals("Cr")) {
- 			crT +=new Double(arr[0]);
- 		} else {
- 			drT +=new Double(arr[0]);
- 		}
- 		
- 		String drTotal = nf.format(drT).toString();
- 		String crTotal = nf.format(crT).toString();
- 		Dailybooks dailyTotal = new Dailybooks("Total",drTotal,crTotal);
- 		dailybooklist.add(dailyTotal);
 
- 		Dailybooks finalTotal;
- 		
- 		if (dailybooklist.size() > 1) {
- 		String lastBal = dailybooklist.get(dailybooklist.size()-2).getBalance();
- 			finalTotal = new Dailybooks("",lastBal,"");
- 		 } else {
- 			finalTotal = new Dailybooks("",nf.format(new Double(arr[0])).toString() ,"");
- 		 }
- 		dailybooklist.add(finalTotal);
- 		ledgerview.setListDailybooks(dailybooklist);
+		ledgerview.setDate(outsdf.format(date1));
+		Double bal = new Double(arr[0]);
+
+		if (arr[1].equals("Cr")) {
+			ledgerview.setdOrC(nf.format(bal).toString());
+			bal *= -1;
+		} else {
+			ledgerview.setOpeningBal(nf.format(bal).toString());
+		}
+		List<Dailybooks> dailybooklist = createDailybooks(code, ledgerForm.getStartDate(), ledgerForm.getEndDate(),
+				bal);
+		Double crT = dailybooklist.stream()
+				.collect(Collectors.summingDouble(d -> new Double(d.getCreditAmt().replace(",", ""))));
+		Double drT = dailybooklist.stream()
+				.collect(Collectors.summingDouble(d -> new Double(d.getDebitAmt().replace(",", ""))));
+
+		if (arr[1].equals("Cr")) {
+			crT += new Double(arr[0]);
+		} else {
+			drT += new Double(arr[0]);
+		}
+
+		String drTotal = nf.format(drT).toString();
+		String crTotal = nf.format(crT).toString();
+		Dailybooks dailyTotal = new Dailybooks("Total", drTotal, crTotal);
+		dailybooklist.add(dailyTotal);
+
+		Dailybooks finalTotal;
+
+		if (dailybooklist.size() > 1) {
+			String lastBal = dailybooklist.get(dailybooklist.size() - 2).getBalance();
+			finalTotal = new Dailybooks("", lastBal, "");
+		} else {
+			finalTotal = new Dailybooks("", nf.format(new Double(arr[0])).toString(), "");
+		}
+		dailybooklist.add(finalTotal);
+		ledgerview.setListDailybooks(dailybooklist);
 		return ledgerview;
 	}
-
 
 	@Override
 	public List<Dailybooks> createDailybooks(Integer code, String startDate, String endDate, Double bal) {
 		List<Dailybooks> dailybooklist = new LinkedList<Dailybooks>();
- 		List<Daybook> daybooks = daybookRepo.findDaybookByAccCodeAndDate(code, startDate, endDate);
- 		for(int i = 0; i<daybooks.size(); i++) {
- 			String arr2[] = {"",""};
- 			if(daybooks.get(i).getDrAmt() == 0) {
- 				bal -= daybooks.get(i).getCrAmt();
- 				if (bal > 0d) {
- 					arr2[0] = nf.format(bal).toString();
- 					arr2[1] = "Dr";
- 					
- 				} else {
- 					arr2[0] = nf.format(Math.abs(bal)).toString();
- 					arr2[1] = "Cr";
- 				}
- 			} else {
- 				bal += daybooks.get(i).getDrAmt(); 
- 				if (bal > 0d) {
- 					arr2[0] = nf.format(bal).toString(); 
- 					arr2[1] = "Dr";
- 				} else {
- 					arr2[0] = nf.format(Math.abs(bal)).toString();
- 					arr2[1] = "Cr";
- 				}
- 			}
- 			Dailybooks dailybook = 
- 					new Dailybooks(daybooks.get(i).getDate(), daybooks.get(i).getNarration(), nf.format(daybooks.get(i).getDrAmt()).toString(),
- 							nf.format(daybooks.get(i).getCrAmt()).toString(),arr2[0],arr2[1]);
- 			dailybooklist.add(dailybook);
- 		}
+		List<Daybook> daybooks = daybookRepo.findDaybookByAccCodeAndDate(code, startDate, endDate);
+		for (int i = 0; i < daybooks.size(); i++) {
+			String arr2[] = { "", "" };
+			if (daybooks.get(i).getDrAmt() == 0) {
+				bal -= daybooks.get(i).getCrAmt();
+				if (bal > 0d) {
+					arr2[0] = nf.format(bal).toString();
+					arr2[1] = "Dr";
+
+				} else {
+					arr2[0] = nf.format(Math.abs(bal)).toString();
+					arr2[1] = "Cr";
+				}
+			} else {
+				bal += daybooks.get(i).getDrAmt();
+				if (bal > 0d) {
+					arr2[0] = nf.format(bal).toString();
+					arr2[1] = "Dr";
+				} else {
+					arr2[0] = nf.format(Math.abs(bal)).toString();
+					arr2[1] = "Cr";
+				}
+			}
+			Dailybooks dailybook = new Dailybooks(daybooks.get(i).getDate(), daybooks.get(i).getNarration(),
+					nf.format(daybooks.get(i).getDrAmt()).toString(), nf.format(daybooks.get(i).getCrAmt()).toString(),
+					arr2[0], arr2[1]);
+			dailybooklist.add(dailybook);
+		}
 		return dailybooklist;
 	}
 
 	private String[] findOpeningBal(Integer code, LedgerForm ledgerForm) {
-		String arr[] = {"",""};
-		if(ledgerForm.getStartDate().equals("2020-04-01")) {
+		String arr[] = { "", "" };
+		if (ledgerForm.getStartDate().equals("2020-04-01")) {
 			Double d1 = accHeadRepo.findCrAmt(code);
 			Double d2 = accHeadRepo.findDrAmt(code);
-			if( d1 == 0d) {
+			if (d1 == 0d) {
 				arr[0] = nf.format(d2).toString();
 				arr[1] = "Dr";
-			}
-			else {
+			} else {
 				arr[0] = nf.format(d1).toString();
 				arr[1] = "Cr";
 			}
-			
+
 		} else {
-			Double d1 = accHeadRepo.findCrAmt(code); 
+			Double d1 = accHeadRepo.findCrAmt(code);
 			Double d2 = accHeadRepo.findDrAmt(code);
-			if( d1 == 0d) {
-				Double tmp = daybookRepo.openBal(code, ledgerForm.getStartDate(), ledgerForm.getEndDate()) ;
+			if (d1 == 0d) {
+				Double tmp = daybookRepo.openBal(code, ledgerForm.getStartDate(), ledgerForm.getEndDate());
 				// If tmp is +ve Dr else Cr
 				if (tmp > 0d || tmp == 0d) {
 					tmp = d2 + tmp;
@@ -186,40 +188,39 @@ public class LedgerServiceImpl implements LedgerService {
 					arr[1] = "Dr";
 				} else {
 					d2 = d2 + tmp;
-						if (d2 > 0d) {
-							arr[0]  = nf.format(d2).toString();
-							arr[1] = "Cr";
-						} else {
-							d2 *= -1;
-							arr[0] = nf.format(d2).toString();
-							arr[1] = "Dr";
-						}
+					if (d2 > 0d) {
+						arr[0] = nf.format(d2).toString();
+						arr[1] = "Cr";
+					} else {
+						d2 *= -1;
+						arr[0] = nf.format(d2).toString();
+						arr[1] = "Dr";
+					}
 				}
-			}
-			else {
+			} else {
 				Double tmp = daybookRepo.openBal(code, ledgerForm.getStartDate(), ledgerForm.getEndDate());
 				// If tmp is +ve Cr else Dr
 				if (tmp > 0d || tmp == 0d) {
 					tmp = d1 + tmp;
 					arr[0] = nf.format(tmp).toString();
-					
+
 					arr[1] = "Cr";
 				} else {
-					
+
 					d1 = d1 + tmp;
-						if (d1 > 0d) {
-							arr[0]  = nf.format(d1).toString();
-							arr[1] = "Dr";
-						} else {
-							d1 *= -1;
-							arr[0] = nf.format(d1).toString();
-							arr[1] = "Cr";
-						}
+					if (d1 > 0d) {
+						arr[0] = nf.format(d1).toString();
+						arr[1] = "Dr";
+					} else {
+						d1 *= -1;
+						arr[0] = nf.format(d1).toString();
+						arr[1] = "Cr";
+					}
 				}
-				
+
 			}
-			
-	}
+
+		}
 		return arr;
 	}
 }
