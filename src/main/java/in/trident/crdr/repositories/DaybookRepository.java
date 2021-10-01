9c27437930bc3256eca44bf90bd6fb7926faa1f0
Nandha <nandha.kumar790@gmail.com>
2021-10-01 23:45:49 +0530

    minor text alignment

--- src/main/java/in/trident/crdr/repositories/DaybookRepository.java
+++ src/main/java/in/trident/crdr/repositories/DaybookRepository.java
@@ -1,4 +1,5 @@
 package in.trident.crdr.repositories;
+
 /**
  * @author Nandhakumar Subramanian
  * 
@@ -17,28 +18,28 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import in.trident.crdr.entities.Daybook;
+
 @Repository
 public interface DaybookRepository extends JpaRepository<Daybook, Long> {
-	
+
 	@Query("select u from Daybook u where u.date = ?1")
 	public ArrayList<Daybook> findDaybookByDate(String date);
-	
+
 	@Query("Select u from Daybook u where u.date between ?1 and ?2")
 	public ArrayList<Daybook> findDaybookRange(String d1, String d2);
 
-	
 	@Query("Select u from Daybook u where u.acccode = ?1 and u.date between ?2 and ?3 order by u.date asc")
 	public List<Daybook> findDaybookByAccCodeAndDate(int accCode, String d1, String d2);
-	
+
 	@Query("select date, sum(crAmt) as crTot, sum(drAmt) as drTot, sum(crAmt)-sum(drAmt) as dayBal, DAYNAME(?1) as dayOfWeek from Daybook u where u.date = ?1")
 	public List<Object[]> findDaybookBalance(String d1);
-	
+
 	@Query(value = "select datediff(?1,?2)", nativeQuery = true)
 	public int findDaysBetween(String d1, String d2);
-	
-	@Query(value="select dayname(?1)", nativeQuery = true)
+
+	@Query(value = "select dayname(?1)", nativeQuery = true)
 	public String findDayOfWeek(String date);
-	
+
 	@Query("select sum(crAmt)-sum(drAmt) as bal from Daybook d where d.acccode = ?1 and d.date between ?2 and ?3")
 	public Double openBal(int acccode, String d1, String d2);
 }
