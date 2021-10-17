4775dd680a891db8c51469894495db63c0de04e5
Nandha <nandha.kumar790@gmail.com>
2021-10-17 22:04:06 +0530

    updated to 4 files upload

--- src/main/java/in/trident/crdr/models/CSVUploadModel.java
+++ src/main/java/in/trident/crdr/models/CSVUploadModel.java
@@ -11,14 +11,45 @@ import org.springframework.web.multipart.MultipartFile;
  */
 public class CSVUploadModel {
 
-	private MultipartFile file;
+	private MultipartFile file1,file2,file3,file4;
 
-	public MultipartFile getFile() {
-		return file;
+	public MultipartFile getFile1() {
+		return file1;
 	}
 
-	public void setFile(MultipartFile file) {
-		this.file = file;
+	public void setFile1(MultipartFile file1) {
+		this.file1 = file1;
 	}
+
+	public MultipartFile getFile2() {
+		return file2;
+	}
+
+	public void setFile2(MultipartFile file2) {
+		this.file2 = file2;
+	}
+
+	public MultipartFile getFile3() {
+		return file3;
+	}
+
+	public void setFile3(MultipartFile file3) {
+		this.file3 = file3;
+	}
+
+	public MultipartFile getFile4() {
+		return file4;
+	}
+
+	public void setFile4(MultipartFile file4) {
+		this.file4 = file4;
+	}
+
+	@Override
+	public String toString() {
+		return "CSVUploadModel [file1=" + file1 + ", file2=" + file2 + ", file3=" + file3 + ", file4=" + file4 + "]";
+	}
+
+		
 	
 }
