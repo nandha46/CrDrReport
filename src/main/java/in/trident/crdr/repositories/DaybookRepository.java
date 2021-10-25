7981e48b72a96743a8668e615a6c48f21b1ef2c4
Nandha <nandha.kumar790@gmail.com>
2021-10-25 22:10:22 +0530

    Repo updated with userid

--- src/main/java/in/trident/crdr/repositories/DaybookRepository.java
+++ src/main/java/in/trident/crdr/repositories/DaybookRepository.java
@@ -22,14 +22,14 @@ import in.trident.crdr.entities.Daybook;
 @Repository
 public interface DaybookRepository extends JpaRepository<Daybook, Long> {
 
-	@Query("select u from Daybook u where u.date = ?1")
-	public ArrayList<Daybook> findDaybookByDate(String date);
+	@Query("select u from Daybook u where u.date = ?1 and u.userid = ?2")
+	public ArrayList<Daybook> findDaybookByDate(String date, Long userid);
 
 	@Query("Select u from Daybook u where u.date between ?1 and ?2")
 	public ArrayList<Daybook> findDaybookRange(String d1, String d2);
 
-	@Query("Select u from Daybook u where u.acccode = ?1 and u.date between ?2 and ?3 order by u.date asc")
-	public List<Daybook> findDaybookByAccCodeAndDate(int accCode, String d1, String d2);
+	@Query("Select u from Daybook u where u.acccode = ?1 and u.userid =?3 and u.date between ?2 and ?3 order by u.date asc")
+	public List<Daybook> findDaybookByAccCodeAndDate(int accCode, String d1, String d2, Long userid);
 
 	@Query("select date, sum(crAmt) as crTot, sum(drAmt) as drTot, sum(crAmt)-sum(drAmt) as dayBal, DAYNAME(?1) as dayOfWeek from Daybook u where u.date = ?1")
 	public List<Object[]> findDaybookBalance(String d1);
@@ -40,6 +40,6 @@ public interface DaybookRepository extends JpaRepository<Daybook, Long> {
 	@Query(value = "select dayname(?1)", nativeQuery = true)
 	public String findDayOfWeek(String date);
 
-	@Query("select sum(crAmt)-sum(drAmt) as bal from Daybook d where d.acccode = ?1 and d.date between ?2 and ?3")
-	public Double openBal(int acccode, String d1, String d2);
+	@Query("select sum(crAmt)-sum(drAmt) as bal from Daybook d where d.acccode = ?1 and d.userid = ?4 and d.date between ?2 and ?3")
+	public Double openBal(int acccode, String d1, String d2, Long userid);
 }
--- src/main/java/in/trident/crdr/repositories/UserRepository.java
+++ src/main/java/in/trident/crdr/repositories/UserRepository.java
@@ -11,4 +11,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	@Query("SELECT u FROM User u WHERE u.email = ?1" )
 	public User findByEmail (String email);
 	
+	
 }
