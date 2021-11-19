05b3571214f8282666244457e99be8e37b8594e4
Nandha <nandha.kumar790@gmail.com>
2021-11-19 22:02:04 +0530

    xml format changed

--- src/main/java/in/trident/crdr/services/PdfService.java
+++ src/main/java/in/trident/crdr/services/PdfService.java
@@ -1,6 +1,7 @@
 package in.trident.crdr.services;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.List;
@@ -11,10 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
 
-import in.trident.crdr.entities.Daybook;
 import in.trident.crdr.models.LedgerForm;
 import in.trident.crdr.models.LedgerView;
-import in.trident.crdr.repositories.DaybookRepository;
+import net.sf.jasperreports.engine.JREmptyDataSource;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperExportManager;
@@ -22,6 +22,8 @@ import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
 
 @Service
 public class PdfService {
@@ -29,26 +31,20 @@ public class PdfService {
 	@Autowired
 	private LedgerService ledgerService;
 	
-	@Autowired
-	private DaybookRepository daybookRepo;
-	
 	public String exportPdf(LedgerForm ledgerForm, Long id) throws FileNotFoundException, JRException {
-	//	List<LedgerView> ledgers = ledgerService.createLedgerViewList(ledgerForm, id);
-		List<Daybook> daybooks = daybookRepo.findDaybookRange("2018-04-01", "2019-03-31");
-		File file = ResourceUtils.getFile("classpath:daybooks.jrxml");
-		JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
-		JRBeanCollectionDataSource datasource = new JRBeanCollectionDataSource(daybooks);
+		List<LedgerView> ledgers = ledgerService.createLedgerViewList(ledgerForm, id);
+		File file = ResourceUtils.getFile("classpath:Blank_A4.jrxml");
+		JasperDesign jd = JRXmlLoader.load(new FileInputStream(file));
+		JasperReport jasperReport = JasperCompileManager.compileReport(jd);
+		JRBeanCollectionDataSource datasource = new JRBeanCollectionDataSource(ledgers);
 		Map<String, Object> parameters = new HashMap<>();
 		parameters.put("CompanyName", "ZODIAC ENTERPRISES (2018-19)");
 		parameters.put("Address", "#12, Main Road, Madurai - 625 001.");
-	//	parameters.put("LedgerListParam", ledgers);
-	//	parameters.put("Parameter1", daybooks);
-	//	JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters , datasource);
-		JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, datasource);
-		Random random = new Random();
-		int x = random.nextInt();
-		JasperExportManager.exportReportToPdfFile(jasperPrint,"C:\\Users\\nandh\\OneDrive\\Desktop\\Pdfs\\ledger_"+x+".pdf");
+		parameters.put("LedgerListParam", datasource);
 		
+		JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters , new JREmptyDataSource());
+		int x = new Random().nextInt();
+		JasperExportManager.exportReportToPdfFile(jasperPrint,"C:\\Users\\nandh\\OneDrive\\Desktop\\Pdfs\\ledger_"+x+".pdf");
 		return "Report generated";
 	}
 }
