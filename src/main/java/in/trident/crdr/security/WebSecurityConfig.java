aee01bb494a2ee2145358c5495ce7efd2840b694
Nandha <nandha.kumar790@gmail.com>
2021-11-26 18:20:17 +0530

    antmatchers updated in websecurity

--- src/main/java/in/trident/crdr/security/WebSecurityConfig.java
+++ src/main/java/in/trident/crdr/security/WebSecurityConfig.java
@@ -53,7 +53,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers("/register","/process_register","/","/css/**","/img/**").permitAll()
 			.antMatchers("/edit/**").hasAnyAuthority("admin")
 			.antMatchers("/daybooks","/ledger","/trial").hasAnyAuthority("developer")
-			.antMatchers("/findDaybook","/findLedger","/findTrialBal","/findTradingPL","/findBalSheet","/reports","/upload","/uploadExcel","/profile","/success","/companies","/company").hasAnyAuthority("developer")
+			.antMatchers("/findDaybook","/findLedger","/findTrialBal","/findTradingPL","/findBalSheet","/reports","/upload","/uploadExcel","/profile","/success","/companies","/company","/loadyears","/StoreCompany","/company_selection","/companyselect").hasAnyAuthority("developer")
 			.antMatchers("/users").hasAnyAuthority("developer")
 			.antMatchers("/delete/**").hasAnyAuthority("developer","client")
 			.anyRequest().authenticated()
