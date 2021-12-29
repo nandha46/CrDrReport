f7cd5cbd7685d84b981a275f2e6a262e4203ce57
Nandha <nandha.kumar790@gmail.com>
2021-12-29 18:16:04 +0530

    improved

--- src/main/java/in/trident/crdr/controllers/AppController.java
+++ src/main/java/in/trident/crdr/controllers/AppController.java
@@ -59,6 +59,7 @@ import in.trident.crdr.services.LedgerService;
 import in.trident.crdr.services.PdfService;
 import in.trident.crdr.services.TradingPLService;
 import in.trident.crdr.services.TrialBalService;
+import in.trident.crdr.util.CSVUtil;
 import net.sf.jasperreports.engine.JRException;
 
 /**
@@ -74,7 +75,7 @@ import net.sf.jasperreports.engine.JRException;
 public class AppController {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(AppController.class);
-	
+
 	private static final DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
 	@Autowired
@@ -103,10 +104,10 @@ public class AppController {
 
 	@Autowired
 	private CloseBalRepo closeBalRepo;
-	
+
 	@Autowired
 	private CompanyService companyService;
-	
+
 	@Autowired
 	private CompSelectionRepo csr;
 
@@ -118,6 +119,12 @@ public class AppController {
 
 	// TODO need to implement user specific table data and filtering system
 
+	@GetMapping("/access")
+	public void runDB() {
+		CSVUtil csv = new CSVUtil();
+		csv.processAccessDatabse();
+	}
+	
 	@GetMapping("/")
 	public String showHomePage(Model model) {
 		model.addAttribute("pageTitle", "CrDr Home");
@@ -125,44 +132,45 @@ public class AppController {
 	}
 
 	@GetMapping("/login")
-	public String showlogin(Model model){
+	public String showlogin(Model model) {
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		LOGGER.info("Inside login controller");
-		if(auth == null || auth instanceof AnonymousAuthenticationToken) {
+		if (auth == null || auth instanceof AnonymousAuthenticationToken) {
 			LOGGER.info("Anonymous user");
 			return "login";
 		}
 		LOGGER.info("Redirection");
 		return "redirect:/company_selection";
 	}
-	
+
 	@GetMapping("/company_selection")
-	public String showCompanies(Model model,  @AuthenticationPrincipal CustomUserDetails user) {
+	public String showCompanies(Model model, @AuthenticationPrincipal CustomUserDetails user) {
 		LOGGER.info("show company accessed");
-		model.addAttribute("pageTitle","Select Company");
-		model.addAttribute("companies",companyService.listCompanies(user.getId()));
+		model.addAttribute("pageTitle", "Select Company");
+		model.addAttribute("companies", companyService.listCompanies(user.getId()));
 		model.addAttribute("yearCriteria", new YearCriteria());
 		return "company_select";
 	}
-	
+
 	@PostMapping("/companyselect")
 	public String showYears(Model model, YearCriteria yearCriteria) {
-		LOGGER.info("Company Name: "+yearCriteria.getCompanyName());
+		LOGGER.info("Company Name: " + yearCriteria.getCompanyName());
 		Map<Long, String> years = companyService.listYears(yearCriteria.getCompanyName());
 		LOGGER.info(years.toString());
-		model.addAttribute("pageTitle","Select Year");
-		model.addAttribute("years",years);
+		model.addAttribute("pageTitle", "Select Year");
+		model.addAttribute("years", years);
 		model.addAttribute("companySelectCriteria", new CompanySelectCriteria());
 		return "year_select";
 	}
-	
+
 	@PostMapping("/StoreCompany")
-	public String processCompany(Model model, CompanySelectCriteria csc, @AuthenticationPrincipal CustomUserDetails user) {
+	public String processCompany(Model model, CompanySelectCriteria csc,
+			@AuthenticationPrincipal CustomUserDetails user) {
 		LOGGER.info("Store company accessed");
-		companyService.storeSelection(user.getId(),csc.getCid());
+		companyService.storeSelection(user.getId(), csc.getCid());
 		return "redirect:/reports";
 	}
-	
+
 	@GetMapping("/profile")
 	public String showProfile(Model model) {
 		model.addAttribute("pageTitle", "Company Profile");
@@ -226,7 +234,7 @@ public class AppController {
 	public String listDaybook(Model model, DaybookForm formdata, @AuthenticationPrincipal CustomUserDetails user) {
 		LOGGER.info("Inside daybooks method");
 		List<DaybookView> daybookViewObj = daybookService.daybookViewRange(formdata.getStartDate(),
-				formdata.getEndDate(), user.getId(),csr.findCompanyIdByUserId(user.getId()));
+				formdata.getEndDate(), user.getId(), csr.findCompanyIdByUserId(user.getId()));
 		model.addAttribute("daybookViewObj", daybookViewObj);
 		Calendar cal = Calendar.getInstance();
 		try {
@@ -236,9 +244,14 @@ public class AppController {
 			LOGGER.info("Calendar Parsing Exception: Daybook Start date");
 		}
 		cal.add(Calendar.DAY_OF_MONTH, -1);
-		model.addAttribute("openingBal",
-				nf.format(closeBalRepo.findCloseBalByDate(new SimpleDateFormat("yyyy-MM-dd").format(cal.getTime()),user.getId(),csr.findCompanyIdByUserId(user.getId()))))
-				.toString();
+		try {
+			model.addAttribute("openingBal",
+					nf.format(closeBalRepo.findCloseBalByDate(new SimpleDateFormat("yyyy-MM-dd").format(cal.getTime()),
+							user.getId(), csr.findCompanyIdByUserId(user.getId()))))
+					.toString();
+		} catch (NullPointerException e) {
+			LOGGER.error("No Close balance in databse", e);
+		}
 		model.addAttribute("pageTitle", "Daybook View");
 		return "daybooks";
 	}
@@ -248,7 +261,7 @@ public class AppController {
 		CompanySelection cs = csr.findCompanyByUser(user.getId());
 		model.addAttribute("fromdate", cs.getFromDate().format(dateFormat));
 		model.addAttribute("todate", cs.getToDate().format(dateFormat));
-		List<AccHead> accHeadList = accHeadRepo.findAllAccHead(user.getId(),csr.findCompanyIdByUserId(user.getId()));
+		List<AccHead> accHeadList = accHeadRepo.findAllAccHead(user.getId(), csr.findCompanyIdByUserId(user.getId()));
 		Collections.sort(accHeadList);
 		LOGGER.info("Loading Ledgers...");
 		model.addAttribute("accHeadList", accHeadList);
@@ -260,14 +273,16 @@ public class AppController {
 	@PostMapping("/ledger")
 	public String listLedger(Model model, LedgerForm ledgerForm, @AuthenticationPrincipal CustomUserDetails user) {
 		LOGGER.info("Ledger is Ready");
-		List<LedgerView> listLedger = ledgerService.createLedgerViewList(ledgerForm, user.getId(),csr.findCompanyIdByUserId(user.getId()));
+		List<LedgerView> listLedger = ledgerService.createLedgerViewList(ledgerForm, user.getId(),
+				csr.findCompanyIdByUserId(user.getId()));
 		model.addAttribute("listLedger", listLedger);
 		model.addAttribute("pageTitle", "CrDr Ledger");
 		return "ledger";
 	}
-	
+
 	@PostMapping("/pdfLedger")
-	public String ledgerPdf(Model model, LedgerForm ledgerForm, @AuthenticationPrincipal CustomUserDetails user) throws FileNotFoundException, JRException {
+	public String ledgerPdf(Model model, LedgerForm ledgerForm, @AuthenticationPrincipal CustomUserDetails user)
+			throws FileNotFoundException, JRException {
 		LOGGER.info("---Printing Ledger to PDF--");
 		pdfService.exportPdf(ledgerForm, user.getId(), csr.findCompanyIdByUserId(user.getId()));
 		return "success";
@@ -275,7 +290,7 @@ public class AppController {
 
 	@GetMapping("/findTrialBal")
 	public String findTrial(Model model, @AuthenticationPrincipal CustomUserDetails user) {
-		List<AccHead> accHeadList = accHeadRepo.findAllAccHead(user.getId(),csr.findCompanyIdByUserId(user.getId()));
+		List<AccHead> accHeadList = accHeadRepo.findAllAccHead(user.getId(), csr.findCompanyIdByUserId(user.getId()));
 		Collections.sort(accHeadList);
 		CompanySelection cs = csr.findCompanyByUser(user.getId());
 		model.addAttribute("fromdate", cs.getFromDate().format(dateFormat));
@@ -290,7 +305,8 @@ public class AppController {
 	@PostMapping("/trial")
 	public String trialbal(Model model, TrialForm trialform, @AuthenticationPrincipal CustomUserDetails user) {
 		LOGGER.warn("Trial Balance Page Start");
-		List<TrialView> listTrailView = trialBalService.createTrialBal(trialform, user.getId(),csr.findCompanyIdByUserId(user.getId()));
+		List<TrialView> listTrailView = trialBalService.createTrialBal(trialform, user.getId(),
+				csr.findCompanyIdByUserId(user.getId()));
 		LOGGER.info("Trial Balnce is Ready");
 		model.addAttribute("listTrailView", listTrailView);
 		model.addAttribute("pageTitle", "Trial Balance");
@@ -299,7 +315,8 @@ public class AppController {
 
 	@GetMapping("/findTradingPL")
 	public String findTradingPl(Model model, @AuthenticationPrincipal CustomUserDetails user) {
-		List<AccHead> accHeadList = accHeadRepo.findAllTradingAccs(user.getId(),csr.findCompanyIdByUserId(user.getId()));
+		List<AccHead> accHeadList = accHeadRepo.findAllTradingAccs(user.getId(),
+				csr.findCompanyIdByUserId(user.getId()));
 		Collections.sort(accHeadList);
 		LOGGER.info("Loading TradingPL...");
 		CompanySelection cs = csr.findCompanyByUser(user.getId());
@@ -315,13 +332,15 @@ public class AppController {
 	@PostMapping("/tradingPL")
 	public String tradingPL(Model model, CommonForm tradingPLForm, @AuthenticationPrincipal CustomUserDetails user) {
 		if (tradingPLForm.isReportOrder()) {
-			List<TradingPLView> listTradingPL = tradingPLService.createTradingPL(tradingPLForm, user.getId(),csr.findCompanyIdByUserId(user.getId()));
+			List<TradingPLView> listTradingPL = tradingPLService.createTradingPL(tradingPLForm, user.getId(),
+					csr.findCompanyIdByUserId(user.getId()));
 			LOGGER.info("TradingPL view  is ready");
 			model.addAttribute("listTradingPL", listTradingPL);
 			model.addAttribute("pageTitle", "Trading - Profit & Loss");
 			return "tradingPL";
 		} else {
-			List<List<TplBalView>> list = tradingPLService.createTradingPL2(tradingPLForm, user.getId(),csr.findCompanyIdByUserId(user.getId()));
+			List<List<TplBalView>> list = tradingPLService.createTradingPL2(tradingPLForm, user.getId(),
+					csr.findCompanyIdByUserId(user.getId()));
 			List<TplBalView> expenses = list.get(0);
 			List<TplBalView> incomes = list.get(1);
 			LOGGER.info("TradingPL Alt view is ready");
@@ -330,12 +349,13 @@ public class AppController {
 			model.addAttribute("pageTitle", "Trading - Profit & Loss");
 			return "tradingPL_alt";
 		}
-		
+
 	}
 
 	@GetMapping("/findBalSheet")
 	public String findBalSheet(Model model, @AuthenticationPrincipal CustomUserDetails user) {
-		List<Schedule> accHeadList = scheduleRepo.findAllAccounts(user.getId(),csr.findCompanyIdByUserId(user.getId()));
+		List<Schedule> accHeadList = scheduleRepo.findAllAccounts(user.getId(),
+				csr.findCompanyIdByUserId(user.getId()));
 		Collections.sort(accHeadList);
 		CompanySelection cs = csr.findCompanyByUser(user.getId());
 		model.addAttribute("fromdate", cs.getFromDate().format(dateFormat));
@@ -349,25 +369,26 @@ public class AppController {
 	}
 
 	@PostMapping("/BalanceSheet")
-	public String balanceSheet(Model model, CommonForm balSheetForm,
-			@AuthenticationPrincipal CustomUserDetails user) {
-		if(balSheetForm.isReportOrder()) {
+	public String balanceSheet(Model model, CommonForm balSheetForm, @AuthenticationPrincipal CustomUserDetails user) {
+		if (balSheetForm.isReportOrder()) {
 			LOGGER.info("Balance Sheet view Ready");
-			List<BalanceSheetView> listBalSheet = balanceSheetService.createBalSheet(balSheetForm, user.getId(),csr.findCompanyIdByUserId(user.getId()));
+			List<BalanceSheetView> listBalSheet = balanceSheetService.createBalSheet(balSheetForm, user.getId(),
+					csr.findCompanyIdByUserId(user.getId()));
 			model.addAttribute("listBalSheet", listBalSheet);
-			model.addAttribute("pageTitle","Balance sheet");
+			model.addAttribute("pageTitle", "Balance sheet");
 			return "BalanceSheet";
 		} else {
 			LOGGER.info("Balance Sheet Alt view  Ready");
-			List<List<TplBalView>> list = balanceSheetService.createBalSheet2(balSheetForm, user.getId(),csr.findCompanyIdByUserId(user.getId()));
+			List<List<TplBalView>> list = balanceSheetService.createBalSheet2(balSheetForm, user.getId(),
+					csr.findCompanyIdByUserId(user.getId()));
 			List<TplBalView> liability = list.get(0);
 			List<TplBalView> asset = list.get(1);
 			model.addAttribute("assets", asset);
 			model.addAttribute("liabilities", liability);
-			model.addAttribute("pageTitle","Balance sheet");
+			model.addAttribute("pageTitle", "Balance sheet");
 			return "balancesheet_alt";
 		}
-		
+
 	}
 
 }
