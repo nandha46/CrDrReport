ea49cd3bcdc60582659c28cc9ae2574edaed98bc
Nandha <nandha.kumar790@gmail.com>
2022-03-14 23:41:40 +0530

    Execution time aspect added

--- src/main/java/in/trident/crdr/annotations/TrackExecutionTime.java
+++ src/main/java/in/trident/crdr/annotations/TrackExecutionTime.java
@@ -20,6 +20,6 @@ import java.lang.annotation.Target;
  */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface ExcecutionTimeTracker {
+public @interface TrackExecutionTime {
 
 }
