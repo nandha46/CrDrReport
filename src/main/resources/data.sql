228171dd134911baf736383d2f0471badcf682b0
Nandha <nandha.kumar790@gmail.com>
2022-03-15 23:46:25 +0530

    queries added

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,2 @@
-INSERT INTO user (email,password,firstName,lastName,enabled) VALUES ('nandha@mail.com','password','nandha','Subramanian','1');
+INSERT INTO user (id,email,password,firstName,lastName,enabled) VALUES ('1','nandha@mail.com','password','nandha','Subramanian','1');
+INSERT INTO role (roleId,roleName) values ('1','developer');
