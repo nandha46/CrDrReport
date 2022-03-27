887fc374407335253e39a02cdbc1a9290a86aa8a
Nandha <nandha.kumar790@gmail.com>
2022-03-27 20:05:42 +0530

    Config updates

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
-INSERT INTO user (id,email,password,firstName,lastName,enabled) VALUES ('1','nandha@mail.com','password','nandha','Subramanian','1');
-INSERT INTO role (roleId,roleName) values ('1','developer');
+INSERT INTO users (id,email,password,first_name,last_name,enabled) VALUES ('1','nandha@mail.com','$2a$10$E.nXRSPx6vadp9wOrQwMLOOettOskYpORmoTA6e2pmwzqUdt2F6Fu','nandha','Subramanian',1);
+INSERT INTO roles (role_id,role_name) values ('1','developer');
+INSERT INTO users_roles (user_id,role_id) values ('1','1');
