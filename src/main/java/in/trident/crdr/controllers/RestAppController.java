66e996f477c3c6f9abb69bf50c1171e517fdcfb9
Nandha <nandha.kumar790@gmail.com>
2022-04-15 11:18:14 +0530

    Navbar and yearselect
    - navbar bootstrap updated
    - new rest api to get years added
    - new js file added with jquery ajax

--- src/main/java/in/trident/crdr/controllers/RestAppController.java
+++ src/main/java/in/trident/crdr/controllers/RestAppController.java
@@ -1,24 +1,40 @@
 package in.trident.crdr.controllers;
 
+
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import in.trident.crdr.entities.Company;
 import in.trident.crdr.repositories.CompanyRepo;
+import in.trident.crdr.services.CompanyService;
+import in.trident.crdr.services.CustomUserDetails;
 
 @RestController
 @RequestMapping("/api/v1/")
 public class RestAppController {
+	
+	private CompanyService companyService;
+	private static final Logger LOGGER = LoggerFactory.getLogger(RestAppController.class);
+	
+	public RestAppController(CompanyService companyService) {
+		this.companyService = companyService;
+	}
 
 	@Autowired
 	private CompanyRepo companyRepo;
@@ -38,5 +54,11 @@ public class RestAppController {
 	public String test2(@CookieValue(value = "companyName",defaultValue = "defaultName") Cookie cookie ,HttpServletRequest request) {
 		
 		return cookie.getName()+ " Expires after/on: " + cookie.getMaxAge();
+	}
+	
+	@GetMapping("/years")
+	public ResponseEntity<?> getCompanyYears(@AuthenticationPrincipal CustomUserDetails user, @RequestParam(name = "companyName") String companyName) {
+		Map<Long, String> years = companyService.listYears(companyName, user.getId());
+		return ResponseEntity.ok(years);
 	}
 }
